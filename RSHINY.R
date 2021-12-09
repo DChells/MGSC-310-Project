d1e60683bca60c78883ed0ca17e2643c97b4c1fa
DChells <danielchelling@gmail.com>
2021-12-09 15:33:28 -0800

    Fixed R Shiny Added Complete Gradient Boosting model
    Added Gradient Boosting Model

--- RSHINY.R
+++ RSHINY.R
@@ -1,78 +1,40 @@
 library(shiny)
 library(leaflet)
 library(htmltools)
+library(leafgl)
+library(sf)
+
 
 crashes <- read.csv(here("datasets", "US_Accidents_Dec20_updated.csv"))
-
-crashes_Map <- crashes %>% mutate()
 
 ui <- fluidPage(
   
   navbarPage("Crashes", id="nav",
              
              tabPanel("MAP",
-                      leafletOutput("map", width="100%", height= 800),
+                      leafglOutput("map", width="100%", height= 800),
                       
-                      absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                                    draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
-                                    width = 330, height = "auto",
-                                    
-                                    h2("Popup Description"),
-                                    
-                                    selectInput("description", "Description", list("Description", "Street") ),
-                                    ),
                       
                       ),
-             
-             tabPanel("Data explorer",
-                      fluidRow(
-                       column(3,
-                              selectInput("states", "States", c("All states"="", structure(state.abb, names=state.name), "Washington, DC"="DC"), multiple=TRUE)
-                       ),
-                       column(3,
-                              conditionalPanel("input.states",
-                                               selectInput("cities", "Cities", c("All cities"=""), multiple=TRUE)
-                              )
-                       ),
-                       column(3,
-                              conditionalPanel("input.states",
-                                               selectInput("zipcodes", "Zipcodes", c("All zipcodes"=""), multiple=TRUE)
-                              )
-                       )
-                     ),
-                     fluidRow(
-                       column(1,
-                              numericInput("minScore", "Min score", min=0, max=100, value=0)
-                       ),
-                       column(1,
-                              numericInput("maxScore", "Max score", min=0, max=100, value=100)
-                              )
-                       )
-                     )
              
              )
   
 )
 
+
+df = st_as_sf(crashes[0:1000,], coords = c("Start_Lng", "Start_Lat"), crs = 4326)
+
 server <- function(input, output, session) {
   
   output$map <- renderLeaflet({
+
     leaflet() %>%
-      addTiles() %>%
+      addProviderTiles(provider = providers$CartoDB.DarkMatter) %>%
+      addGlPoints(data = df, popup = TRUE) %>% 
       setView(lng = -93.85, lat = 37.45, zoom = 4)
   })
   
-  observe({
-    description <- input$description
-    }
-  )
   
-  output$map <- renderLeaflet ({  
-    leaflet(data = crashes[1:50,]) %>% addTiles() %>%
-      addMarkers(~Start_Lng, ~Start_Lat, popup = ~htmlEscape(description))
-  })
-
 }
 
 shinyApp(ui, server)
-
